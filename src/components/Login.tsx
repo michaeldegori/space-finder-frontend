7bf2880b7f89d08dfe373c61df9dd5113c73a36f
Michael DeGori <michael.degori@gmail.com>
2023-02-04 17:08:42 -0500

    implement login functionality

---
+++ src/components/Login.tsx
@@ -0,0 +1,79 @@
+import React, { SyntheticEvent } from 'react';
+import { AuthService } from '../services/AuthService';
+
+interface LoginProps {
+	authService: AuthService;
+}
+
+interface LoginState {
+	userName: string;
+	password: string;
+	loginAttempted: boolean;
+	loginSuccessful: boolean;
+}
+
+interface CustomEvent {
+	target: HTMLInputElement;
+}
+
+export class Login extends React.Component<LoginProps, LoginState> {
+	state: LoginState = {
+		userName: '',
+		password: '',
+		loginAttempted: false,
+		loginSuccessful: false,
+	};
+
+	private setUserName(event: CustomEvent) {
+		this.setState({ userName: event.target.value });
+	}
+	private setPassword(event: CustomEvent) {
+		this.setState({ password: event.target.value });
+	}
+	private async handleSubmit(event: SyntheticEvent) {
+		event.preventDefault();
+		this.setState({ loginAttempted: true });
+		const result = await this.props.authService.login(
+			this.state.userName,
+			this.state.password
+		);
+
+		if (result) {
+			this.setState({ loginSuccessful: true });
+		} else {
+			console.log('Wrong login');
+		}
+	}
+
+	render() {
+		let loginMessage: any;
+		if (this.state.loginAttempted) {
+			if (this.state.loginSuccessful) {
+				loginMessage = <label>Login successful</label>;
+			} else {
+				loginMessage = <label>Login failed</label>;
+			}
+		}
+
+		return (
+			<div>
+				<h2>Please login</h2>
+				<form onSubmit={event => this.handleSubmit(event)}>
+					<input
+						value={this.state.userName}
+						onChange={event => this.setUserName(event)}
+					/>
+					<br />
+					<input
+						value={this.state.password}
+						onChange={event => this.setPassword(event)}
+						type='password'
+					/>
+					<br />
+					<input type='submit' />
+				</form>
+				{loginMessage}
+			</div>
+		);
+	}
+}
